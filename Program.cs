e71885f0dfaf06d0bc968a1814af31897ef5bed9
unknown <souryzetma1144@yahoo.com>
2024-08-12 20:16:23 +0700

    commit 1

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿﻿using System;
+
+namespace dotnet_investment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double money = 1.0;
+            int month = 1;
+            double interset_rate = 1.0;
+
+            Console.WriteLine("Enter investment amount: ");
+            money = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter number of months: ");
+            month = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter annual interest rate in percentage: ");
+            interset_rate = Double.Parse(Console.ReadLine());
+
+            double total_interset = 0;
+            for (int i = 0; i < month; i++)
+            {
+                total_interset = money * (interset_rate / 100) / 12 * 3;
+            }
+
+            Console.WriteLine("Total of interset: " + total_interset);
+        }
+    }
+}
